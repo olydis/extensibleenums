6a09721467f4a613fb8f6f4705380eaef3bc5ade
Johannes Bader <olydis@users.noreply.github.com>
2017-10-31 10:59:38 -0700

    Update TaxAmount.cs

--- TaxAmount.cs
+++ TaxAmount.cs
@@ -7,48 +7,34 @@ namespace EnumTests
             Value = value;
         }
 
-        public static implicit operator TaxAmountEnums(double value) 
-        { 
-            var baseType = GetBaseTypeForAllowedType(value);
-            return (baseType == null)? new TaxAmountEnums(value): (TaxAmountEnums)baseType;
-        }
-
-        private static TaxAmountEnums GetBaseTypeForAllowedType(TaxAmountEnums other)
-        {
-            return new TaxAmountEnums();
-        }        
+        public static implicit operator TaxAmountEnums(double value) => new TaxAmountEnums(value);
 
         public static bool operator ==(TaxAmountEnums m1, TaxAmountEnums m2) => m2.Equals(m1);
 
         public static bool operator !=(TaxAmountEnums m1, TaxAmountEnums m2) => !m2.Equals(m1);
 
-        public override bool Equals(object obj) => this.Equals(((TaxAmountEnums)obj));
+        public override bool Equals(object obj) => obj is TaxAmountEnums e && this.Equals(e);
 
-        public bool Equals(TaxAmountEnums m) => ( GetBaseTypeForAllowedType(m.Value)!=null || Value.Equals(m.Value));
+        public bool Equals(TaxAmountEnums m) => GetBaseValueForAllowedValue(Value).Equals(GetBaseValueForAllowedValue(m.Value));
 
-        public override int GetHashCode()
-        {
-            var baseType = GetBaseTypeForAllowedType(this);
-            if(baseType!=null)
-            {
-                return baseType.Value.GetHashCode();
-            }
-            return Value.GetHashCode();
-        }
+        public override int GetHashCode() => GetBaseValueForAllowedValue(Value).GetHashCode();
 
-        public double Value {get; private set;}
+        public double Value { get; private set; }
 
-        private static TaxAmountEnums? GetBaseTypeForAllowedType(double value)
+        private static double GetBaseValueForAllowedValue(double value)
         {
             switch(value)
             {
-                case 1.1: return new TaxAmountEnums(1.0);
-                case 1.2: return new TaxAmountEnums(1.0);
-                case 1.3: return new TaxAmountEnums(1.3);
-                case 2.1: return new TaxAmountEnums(2.0);
-                case 2.2: return new TaxAmountEnums(2.0);
-                case 3.1: return new TaxAmountEnums(3.0);
-                default: return null;
+                case 1.1:
+                case 1.2:
+                case 1.3:
+                     return 1.0;
+                case 2.1:
+                case 2.2:
+                     return 2.0;
+                case 3.1:
+                     return 3.0;
+                default: return value;
             }
         }
 
